f48af6d0837171be201d99c3da81cd6a287e63c8
Bùi Lê Hoài An <builehoaian2002@gmail.com>
2024-02-16 15:17:24 +0700

    implement route for join room

--- src/main/kotlin/com/anbui/plugins/Routing.kt
+++ src/main/kotlin/com/anbui/plugins/Routing.kt
@@ -2,6 +2,7 @@ package com.anbui.plugins
 
 import com.anbui.routes.createRoomRoute
 import com.anbui.routes.getRoomRoute
+import com.anbui.routes.joinRoomRoute
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
@@ -9,5 +10,6 @@ fun Application.configureRouting() {
     routing {
         createRoomRoute()
         getRoomRoute()
+        joinRoomRoute()
     }
 }
