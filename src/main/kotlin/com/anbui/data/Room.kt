de0422d2a907a18b6777d20de3bdebef0121744c
Bùi Lê Hoài An <builehoaian2002@gmail.com>
2024-02-17 00:21:10 +0700

    Implement game's phase listener

--- src/main/kotlin/com/anbui/data/Room.kt
+++ src/main/kotlin/com/anbui/data/Room.kt
@@ -11,6 +11,47 @@ class Room(
     val maxPlayer: Int,
     var players: List<Player> = listOf()
 ) {
+
+    /**
+     * listener for phase change
+     */
+    private var phaseChangeListener: ((Phase) -> Unit)? = null
+    var phase = Phase.WAITING_FOR_PLAYER
+        private set(value) {
+            synchronized(field) {
+                field = value
+                phaseChangeListener?.let { change ->
+                    change(value)
+                }
+            }
+        }
+
+    private fun setPhaseChangeListener(listener: (Phase) -> Unit) {
+        phaseChangeListener = listener
+    }
+
+    init {
+        setPhaseChangeListener { newPhase ->
+            when (newPhase) {
+                Phase.WAITING_FOR_PLAYER -> {
+                    waitingForPlayer()
+                }
+                Phase.WAITING_FOR_START -> {
+                    waitingForStart()
+                }
+                Phase.NEW_ROUND -> {
+                    newRound()
+                }
+                Phase.GAME_RUNNING -> {
+                    gameRunning()
+                }
+                Phase.SHOW_WORD -> {
+                    showWord()
+                }
+            }
+        }
+    }
+
     /**
      * Send message to all player in room if they are active
      */
@@ -37,5 +78,49 @@ class Room(
         return players.any { player ->
             player.username == username
         }
+    }
+
+    /**
+     *
+     */
+    private fun waitingForPlayer() {
+
+    }
+
+    /**
+     *
+     */
+    private fun waitingForStart() {
+
+    }
+
+    /**
+     *
+     */
+    private fun newRound() {
+
+    }
+
+    /**
+     *
+     */
+    private fun gameRunning() {
+
+    }
+
+    /**
+     *
+     */
+    private fun showWord() {
+
+    }
+
+
+    enum class Phase {
+        WAITING_FOR_PLAYER,
+        WAITING_FOR_START,
+        NEW_ROUND,
+        GAME_RUNNING,
+        SHOW_WORD
     }
 }
